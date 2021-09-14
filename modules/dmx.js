ad49e8ed697a2aa14e6d0f3f8a7ac1fb2825b8f7
Magdi Hazaa <magdihazaa@gmail.com>
2021-09-13 23:58:36 -0500

    dmx

--- modules/dmx.js
+++ modules/dmx.js
@@ -15,3 +15,33 @@ export default class DMXController {
     });
   }
 }*/
+
+/*****/
+
+/*const DMX = require('dmx');
+
+//Enttec Open DMX USB
+
+module.exports = class DMXController {
+  constructor() {
+    const dmx = new DMX();
+    this.universe = dmx.addUniverse('demo', 'enttec-open-usb-dmx', '/dev/cu.usbserial-AB0KU85Q');
+    dmx.update('demo', {1: 200})
+
+  }
+  setColor(startChannel, r, g, b) {
+    this.universe.update({
+      [startChannel]: r,
+      [startChannel + 1]: g,
+      [startChannel + 2]: b
+    });
+  }
+}*/
+
+/*****/
+
+/*const DMX = require('dmx');
+const dmx = new DMX();
+const universe = dmx.addUniverse('demo', 'enttec-open-usb-dmx', '/dev/cu.usbserial-AB0KU85Q');
+// const universe = dmx.addUniverse('demo', 'enttec-open-usb-dmx', '/Device/VCP0');
+dmx.update9('demo', {1: 200});*/
